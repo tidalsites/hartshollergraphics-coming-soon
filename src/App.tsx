58bab276225a245050644b8442a93286c115dce3
Devin Haynes <tidalsites@gmail.com>
2021-07-10 19:08:11 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.scss";
+import icon from "./coming-soon-white.svg";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <span className="App__businessName">Harts Holler Graphics</span>
+      <img src={icon} alt="Coming soon" />
+      <span className="App__subtext">Brought to you by Tidal Sites, LLC</span>
+      <div className="contact">
+        <span>Contact us while this website is being built:</span>
+        <div className="phone">
+          <span>Phone: (870) 405-3822</span>
+        </div>
+        <div className="email">
+          <span>Email: blhart89@outlook.com</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
